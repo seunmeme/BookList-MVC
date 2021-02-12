f695cb62f9db953df3cfcfbf980e5616763e1dc3
Seun Odewale <seunodewale.zone5@gmail.com>
2021-02-12 19:18:06 +0100

    add BookTable migration

---
+++ BookListMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
